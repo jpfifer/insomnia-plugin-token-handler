84513a78e3e32c9da957862a2df6088c5db84c70
Justin Pfifer <justin.pfifer@carvana.com>
2020-07-17 11:25:02 -0700

    Initial creation of the plugin

---
+++ Insomnia.d.ts
@@ -0,0 +1,183 @@
+declare module Insomnia {
+
+  interface BaseTemplateArgument {
+    displayName: string;
+    description?: string;
+    defaultValue?: string | number | boolean | null;
+    type: 'string' | 'number' | 'enum' | 'model';
+  }
+
+  interface StringTemplateArgument extends BaseTemplateArgument {
+    type: 'string';
+    placeholder?: string;
+  }
+
+  interface ModelTemplateArgument extends BaseTemplateArgument {
+    type: 'model';
+    model: string;
+  }
+
+  interface EnumTemplateArgument extends BaseTemplateArgument {
+    type: 'enum';
+    options: [{
+      displayName: string;
+      value: string;
+      description?: string;
+      placeholder?: string;
+    }]
+  }
+
+  type TemplateArgument =
+    BaseTemplateArgument
+    & (StringTemplateArgument | ModelTemplateArgument | EnumTemplateArgument);
+
+  interface TemplateTag {
+    name: string;
+    displayName: string;
+    disablePreview?: () => boolean;
+    description?: string;
+    deprecated?: boolean;
+    liveDisplayName?: (args) => string | null;
+    validate?: (value: any) => string | null;
+    priority?: number;
+    args: TemplateArgument[];
+
+    run(...any): any
+  }
+
+  interface Store {
+    hasItem(key: string): Promise<boolean>;
+    setItem(key: string, value: string): Promise<void>;
+    getItem(key: string): Promise<string | null>;
+    removeItem(key: string): Promise<void>;
+    clear(): Promise<void>;
+    all(): Promise<{ key: string, value: string }[]>;
+  }
+
+  interface BaseModel {
+    _id: string;
+    type: string;
+    parentId: string;
+    modified: number;
+    created: number;
+  }
+
+  interface Network {
+    sendRequest(request: Request.Request): Promise<Response.Response>
+  }
+
+  interface Context {
+    network: Network;
+    store: Store;
+    util: Util.Util;
+  }
+}
+
+declare module Insomnia.Util {
+
+  interface Util {
+    models: {
+      request: Request.RequestUtil,
+      response: Response.ResponseUtil
+    }
+  }
+}
+
+declare module Insomnia.Request {
+
+  interface RequestUtil {
+    getById(id: string): Promise<Request | null>
+  }
+
+  interface RequestHeader {
+    name: string,
+    value: string,
+    disabled?: boolean,
+  }
+
+  interface RequestParameter {
+    name: string,
+    value: string,
+    disabled?: boolean,
+    id?: string,
+    fileName?: string,
+  }
+
+  interface RequestBodyParameter {
+    name: string,
+    value: string,
+    disabled?: boolean,
+    multiline?: string,
+    id?: string,
+    fileName?: string,
+    type?: string,
+  }
+
+  interface RequestBody {
+    mimeType?: string | null,
+    text?: string,
+    fileName?: string,
+    params?: RequestBodyParameter[],
+  }
+
+  type RequestAuthentication = any;
+
+  interface Request extends BaseModel {
+    url: string,
+    name: string,
+    description: string,
+    method: string,
+    body: RequestBody,
+    parameters: Array<RequestParameter>,
+    headers: Array<RequestHeader>,
+    authentication: RequestAuthentication,
+    metaSortKey: number,
+    isPrivate: boolean,
+
+    // Settings
+    settingStoreCookies: boolean,
+    settingSendCookies: boolean,
+    settingDisableRenderRequestBody: boolean,
+    settingEncodeUrl: boolean,
+    settingRebuildPath: boolean,
+    settingMaxTimelineDataSize: number,
+  }
+}
+
+declare module Insomnia.Response {
+  interface ResponseUtil {
+    getLatestForRequestId: (id: string) => Promise<Response | null>,
+    getBodyBuffer: (response: Response, fallback?: any) => Promise<Buffer | null>,
+  }
+
+  interface ResponseHeader {
+    name: string,
+    value: string,
+  }
+
+  interface ResponseTimelineEntry {
+    name: string,
+    value: string,
+  }
+
+  interface Response extends BaseModel {
+    statusCode: number,
+    statusMessage: string,
+    httpVersion: string,
+    contentType: string,
+    url: string,
+    bytesRead: number,
+    bytesContent: number,
+    elapsedTime: number,
+    headers: ResponseHeader[],
+    timeline: ResponseTimelineEntry[],
+    bodyPath: string, // Actual bodies are stored on the filesystem
+    bodyCompression: 'zip' | null | '__NEEDS_MIGRATION__',
+    error: string,
+    requestVersionId: string | null,
+
+    // Things from the request
+    settingStoreCookies: boolean | null,
+    settingSendCookies: boolean | null,
+  }
+}
---
+++ index.ts
@@ -0,0 +1,133 @@
+import * as iconv from 'iconv-lite'
+
+// type BaseTemplateArgument = {
+//   displayName: string;
+//   description?: string;
+//   defaultValue?: string | number | boolean | null;
+//   type: 'string' | 'number' | 'enum' | 'model';
+// };
+//
+// type StringTemplateArgument = {
+//   type: 'string';
+//   placeholder?: string;
+// };
+//
+// type ModelTemplateArgument = {
+//   type: 'model';
+//   model: string;
+// };
+//
+// type EnumTemplateArgument = {
+//   type: 'enum';
+//   options: [{
+//     displayName: string;
+//     value: string;
+//     description?: string;
+//     placeholder?: string;
+//   }]
+// }
+//
+// type TemplateArgument = BaseTemplateArgument & (StringTemplateArgument | ModelTemplateArgument | EnumTemplateArgument);
+//
+// type TemplateTag = {
+//   name: string,
+//   displayName: string,
+//   disablePreview?: () => boolean,
+//   description?: string,
+//   deprecated?: boolean,
+//   liveDisplayName?: (args) => string | null,
+//   validate?: (value: any) => string | null,
+//   priority?: number,
+//   args: TemplateArgument[],
+//   run(...any): any
+// };
+
+type TokenResponse = {
+  access_token: string;
+  expires_in: number;
+  token_type: string;
+}
+
+const processRequest = () => {
+
+}
+
+const decodeResponse = (context, response): TokenResponse => {
+  if (response.statusCode != 200) {
+    throw new Error(`Request completed with non-200 error code: ${response.statusCode}`);
+  }
+
+  const bodyBuffer = context.util.models.response.getBodyBuffer(response, '');
+  const match = response.contentType.match(/charset=([\w-]+)/);
+  const charset = match && match.length >= 2 ? match[1] : 'utf-8';
+
+  let output = '';
+  // Sometimes iconv conversion fails so fallback to regular buffer
+  try {
+    output = iconv.decode(bodyBuffer, charset);
+  } catch (err) {
+    console.warn('[response] Failed to decode body', err);
+    output = bodyBuffer.toString();
+  }
+
+  return JSON.parse(output);
+}
+
+const isOutOfDate = (lastRequest: Date) => {
+  if (!lastRequest) {
+    return true;
+  }
+  const now = Date.now();
+  console.log(`IsOutOfDate: now: ${now}, lastRequest: ${lastRequest.getTime()} => ${now - lastRequest.getTime()}`);
+  return now - lastRequest.getTime() > 3600000;
+}
+
+export const templateTags: Insomnia.TemplateTag[] = [
+  {
+    name: 'AuthToken',
+    displayName: 'Auth Token',
+    description: 'Retrieve an auth token from another request',
+    args: [
+      {
+        displayName: 'Request',
+        type: 'model',
+        model: 'Request'
+      }
+    ],
+    async run(context: Insomnia.Context, requestId) {
+      console.log(`Started running with ${context} and ${requestId}`);
+      if (!requestId) {
+        console.log(`No requestId`);
+        throw new Error('No request provided');
+      }
+
+      const request = await context.util.models.request.getById(requestId);
+      if (!request) {
+        console.log(`No request`);
+        throw new Error(`Could not find request for requestId: ${requestId}`);
+      }
+
+      let response = await context.util.models.response.getLatestForRequestId(requestId);
+      const currentValue = await context.store.getItem(`LastRequest-${request._id}`);
+      const lastUpdated = new Date(currentValue);
+      if (!response || isOutOfDate(lastUpdated) ) {
+        response = await context.network.sendRequest(request);
+        await context.store.setItem(`LastRequest-${request._id}`, new Date().toUTCString());
+      }
+
+      const jwt = decodeResponse(context, response);
+
+      if (jwt) {
+        if (jwt.access_token) {
+          return jwt.access_token;
+        }
+        if (jwt[0]) {
+          return jwt[0]
+        }
+        console.log(`Unable to find a needed field in jwt`, jwt);
+      }
+      console.log(`No token found`);
+      throw new Error("Failed to extract anything");
+    }
+  }
+];
